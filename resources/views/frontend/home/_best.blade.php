b1de2d78f9f13122944abb48e95894d984a9fe4b
HoangPH <hoangph.paraline@gmail.com>
2018-12-04 14:08:31 +0700

    Update home view

--- resources/views/frontend/home/_best.blade.php
+++ resources/views/frontend/home/_best.blade.php
@@ -8,7 +8,8 @@
                 <div class="product-sales-item">
                     <div class="product-sales-img">
                         <a href="http://khuyenmai.hungmobile.vn/sinh-nhat-hungmobile-23-quan-hoa-1-tuoi/">
-                            <img src="./index_files/sinh_nhật_23_5bfc0026070e0_26_11_2018_21_16_06.jpg" style="width: 100%">
+                            <img src="./index_files/sinh_nhật_23_5bfc0026070e0_26_11_2018_21_16_06.jpg"
+                                 style="width: 100%">
                         </a>
                     </div>
                     <div class="product-sales-content">
@@ -27,7 +28,8 @@
                 <div class="product-sales-item">
                     <div class="product-sales-img">
                         <a href="https://hungmobile.vn/galaxy-s7-s7-edge.html">
-                            <img src="./index_files/67e6dda8cf4f2f11765e_5c01063e26567_30_11_2018_16_43_26.jpg" style="width: 100%">
+                            <img src="./index_files/67e6dda8cf4f2f11765e_5c01063e26567_30_11_2018_16_43_26.jpg"
+                                 style="width: 100%">
                         </a>
                     </div>
                     <div class="product-sales-content">
@@ -46,7 +48,8 @@
                 <div class="product-sales-item">
                     <div class="product-sales-img">
                         <a href="https://hungmobile.vn/nokia-x7.html">
-                            <img src="./index_files/b9dc926cfe351d6b4424_5c01052d83de3_30_11_2018_16_38_53.jpg" style="width: 100%">
+                            <img src="./index_files/b9dc926cfe351d6b4424_5c01052d83de3_30_11_2018_16_38_53.jpg"
+                                 style="width: 100%">
                         </a>
                     </div>
                     <div class="product-sales-content">
@@ -67,7 +70,8 @@
                 <div class="product-sales-item">
                     <div class="product-sales-img">
                         <a href="https://hungmobile.vn/galaxy-note-8.html">
-                            <img src="./index_files/0d289e99f2c0119e48d1_5c0104fe7b8cc_30_11_2018_16_38_06.jpg" style="width: 100%">
+                            <img src="./index_files/0d289e99f2c0119e48d1_5c0104fe7b8cc_30_11_2018_16_38_06.jpg"
+                                 style="width: 100%">
                         </a>
                     </div>
                     <div class="product-sales-content">
@@ -86,13 +90,14 @@
                 <div class="product-sales-item">
                     <div class="product-sales-img">
                         <a href="https://hungmobile.vn/timkiem?search=iphone+6s">
-                            <img src="./index_files/675d30ed5cb4bfeae6a5_5c0104e060d87_30_11_2018_16_37_36.jpg" style="width: 100%">
+                            <img src="./index_files/675d30ed5cb4bfeae6a5_5c0104e060d87_30_11_2018_16_37_36.jpg"
+                                 style="width: 100%">
                         </a>
                     </div>
                     <div class="product-sales-content">
                         <h3 style="min-height: 50px;">
                             <a href="https://hungmobile.vn/timkiem?search=iphone+6s">
-                                iPhone 6S Quốc tế Likenew 1 đổi 1 trong cả 15 tháng. 
+                                iPhone 6S Quốc tế Likenew 1 đổi 1 trong cả 15 tháng.
                             </a>
                         </h3>
                         <div class="prod-price">
@@ -104,10 +109,14 @@
             <div class="col-md-4 col-sm-6 col-xs-12">
                 <div class="product-sales-item">
                     <div class="product-sales-img">
-                        <a href="https://hungmobile.vn/iphone-x-64gb-cu-xach-tay-gia-re.html"><img src="./index_files/928d833def640c3a5575_5c01053d2d7e4_30_11_2018_16_39_09.jpg" style="width: 100%"></a>
+                        <a href="https://hungmobile.vn/iphone-x-64gb-cu-xach-tay-gia-re.html">
+                            <img src="./index_files/928d833def640c3a5575_5c01053d2d7e4_30_11_2018_16_39_09.jpg"style="width: 100%">
+                        </a>
                     </div>
                     <div class="product-sales-content">
-                        <h3 style="min-height: 50px;"><a href="https://hungmobile.vn/iphone-x-64gb-cu-xach-tay-gia-re.html">iPhone X Bảo hành 1 đổi 1 trong cả 15 tháng</a></h3>
+                        <h3 style="min-height: 50px;"><a
+                                    href="https://hungmobile.vn/iphone-x-64gb-cu-xach-tay-gia-re.html">iPhone X Bảo hành
+                                1 đổi 1 trong cả 15 tháng</a></h3>
                         <div class="prod-price">
                             <span>17,390,000đ</span>
                         </div>
---
+++ resources/views/frontend/home/_phu_kien.blade.php
@@ -0,0 +1,118 @@
+<div class="ct-heading clearfix">
+    <h2 class="col-md-1 col-sm-3 col-xs-6 cth2">Phụ kiện <!-- <span class="ct-tringle"></span> --></h2>
+    <div class="col-md-11 col-sm-9 col-xs-6 ct-pk-type">
+        <span class="hidden-lg btn-ul">Chọn loại: <i class="fa fa-caret-down"></i></span>
+        <ul class="ulh2">
+            <li><h2><a href="https://hungmobile.vn/op-lung.html">Ốp lưng</a></h2></li>
+            <li><h2><a href="https://hungmobile.vn/bao-da.html">Bao da</a></h2></li>
+            <li><h2><a href="https://hungmobile.vn/sac.html">Sạc</a></h2></li>
+            <li><h2><a href="https://hungmobile.vn/cap-usb.html">Cáp USB</a></h2></li>
+            <li><h2><a href="https://hungmobile.vn/pin-du-phong.html">Pin dự phòng</a></h2></li>
+            <li><h2><a href="https://hungmobile.vn/tai-nghe.html">Tai nghe</a></h2></li>
+            <li><h2><a href="https://hungmobile.vn/kinh-cuong-luc.html">Kính cường lực</a></h2></li>
+            <li><h2><a href="https://hungmobile.vn/dan-man-hinh.html">Dán màn hình</a></h2></li>
+
+        </ul>
+    </div>
+</div>
+<div class="ct-pk">
+    <div class="ct-box-pk">
+        <a href="https://hungmobile.vn/gay-chup-hinh-tu-suong-hang-xin.html"><img
+                    src="./index_files/gay-chup-anh_596aea9838be5_16_07_2017_11_24_56.jpg"
+                    alt="Gậy chụp Sefile Monopod" class="img-responsive"></a>
+        <div class="pk-content">
+            <a href="https://hungmobile.vn/gay-chup-hinh-tu-suong-hang-xin.html" class="pro-name">Gậy
+                chụp Sefile Monopod</a>
+            <p class="new-price">
+
+                80,000đ
+
+            </p>
+        </div>
+    </div>
+    <div class="ct-box-pk">
+        <a href="https://hungmobile.vn/tai-nghe-iphone-chinh-hang.html"><img
+                    src="./index_files/tai-nghe-iphone-jin_594337d411cac_16_06_2017_08_43_48.jpg"
+                    alt="Tai nghe iPhone 6s Chính Hãng Apple" class="img-responsive"></a>
+        <div class="pk-content">
+            <a href="https://hungmobile.vn/tai-nghe-iphone-chinh-hang.html" class="pro-name">Tai nghe
+                iPhone 6s Chính Hãng Apple</a>
+            <p class="new-price">
+
+                360,000đ
+
+            </p>
+        </div>
+    </div>
+    <div class="ct-box-pk">
+        <a href="https://hungmobile.vn/bao-da-passposrt-chinh-hang.html"><img
+                    src="./index_files/bao-da-blackbery-passpost(06)_5942744b2bdbf_15_06_2017_18_49_31.jpg"
+                    alt="Bao da passposrt chính hãng" class="img-responsive"></a>
+        <div class="pk-content">
+            <a href="https://hungmobile.vn/bao-da-passposrt-chinh-hang.html" class="pro-name">Bao da
+                passposrt chính hãng</a>
+            <p class="new-price">
+
+                220,000đ
+
+            </p>
+        </div>
+    </div>
+    <div class="ct-box-pk">
+        <a href="https://hungmobile.vn/cap-usb-jellico.html"><img
+                    src="./index_files/cap-usb-jellico_59410d81aef0d_14_06_2017_17_18_41.jpg"
+                    alt="cáp usb jellico chính hãng" class="img-responsive"></a>
+        <div class="pk-content">
+            <a href="https://hungmobile.vn/cap-usb-jellico.html" class="pro-name">cáp usb jellico chính
+                hãng</a>
+            <p class="new-price">
+
+                150,000đ
+
+            </p>
+        </div>
+    </div>
+    <div class="ct-box-pk">
+        <a href="https://hungmobile.vn/tai-nghe-sony-extra-bass-mdr-xb450ap.html"><img
+                    src="./index_files/tai-nghe-sony-extra-bass-mdr-xb450ap_594102c1da638_14_06_2017_16_32_49.jpg"
+                    alt="Tai nghe sony extra bass mdr-xb450ap" class="img-responsive"></a>
+        <div class="pk-content">
+            <a href="https://hungmobile.vn/tai-nghe-sony-extra-bass-mdr-xb450ap.html" class="pro-name">Tai
+                nghe sony extra bass mdr-xb450ap</a>
+            <p class="new-price">
+
+                200,000đ
+
+            </p>
+        </div>
+    </div>
+    <div class="ct-box-pk">
+        <a href="https://hungmobile.vn/jack-chuyen-tai-nghe-iphone-7-titan.html"><img
+                    src="./index_files/jax-chuyen-doi-tu-cong-lightning-sang-cong-tai-nghe-3,5-mm_5940ed8fa3922_14_06_2017_15_02_23.jpg"
+                    alt="jack chuyển tai nghe Iphone 7 TITAN" class="img-responsive"></a>
+        <div class="pk-content">
+            <a href="https://hungmobile.vn/jack-chuyen-tai-nghe-iphone-7-titan.html" class="pro-name">jack
+                chuyển tai nghe Iphone 7 TITAN</a>
+            <p class="new-price">
+
+                90,000đ
+
+            </p>
+        </div>
+    </div>
+    <div class="ct-box-pk">
+        <a href="https://hungmobile.vn/sac-khong-day-samsung-chinh-hang.html"><img
+                    src="./index_files/sac-khong-day-samsung_5940e249aaa88_14_06_2017_14_14_17.jpg"
+                    alt="Sạc không dây Samsung chính hãng" class="img-responsive"></a>
+        <div class="pk-content">
+            <a href="https://hungmobile.vn/sac-khong-day-samsung-chinh-hang.html" class="pro-name">Sạc
+                không dây Samsung chính hãng</a>
+            <p class="new-price">
+
+                360,000đ
+
+            </p>
+        </div>
+    </div>
+
+</div>
