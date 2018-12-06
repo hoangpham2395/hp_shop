29af841cd62f9f477d80f9ed0b5cecad3e5f6fea
HoangPH <hoangph.paraline@gmail.com>
2018-12-06 11:04:00 +0700

    [Backend] Update product option

--- resources/views/backend/products/_product_option.blade.php
+++ resources/views/backend/products/_product_option.blade.php
@@ -2,7 +2,7 @@
     <div class="col-md-12 col-sm-12 col-xs-12">
         <div class="box box-danger">
             <div class="box-header">
-                <h3 class="box-title">{{getTitle('products.option')}}</h3>
+                <h3 class="box-title">{{getTitle('products.option')}} <span class="panel_heading">{{ is_numeric($index) ? $index + 1 : $index}}</span></h3>
             </div>
             <div class="box-body">
                 <div class="row">
@@ -11,7 +11,7 @@
                             {!! Form::label('color', getTitle('products.color')) !!}
                             <div class="input-group">
                                 <span class="input-group-addon"><i class="fa fa-adjust"></i></span>
-                                {!! Form::text('color', null, ['class' => 'form-control', 'placeholder' => getTitle('products.color')]) !!}
+                                {!! Form::text('ProductPrice['.$index.'][color]', null, ['class' => 'form-control', 'placeholder' => getTitle('products.color')]) !!}
                             </div>
                         </div>
                     </div>
@@ -20,7 +20,7 @@
                             {!! Form::label('price', getTitle('products.price')) !!}
                             <span class="required"></span>
                             <div class="input-group">
-                                {!! Form::text('price', null, ['class' => 'form-control', 'placeholder' => getTitle('products.price')]) !!}
+                                {!! Form::text('ProductPrice['.$index.'][price]', null, ['class' => 'form-control', 'placeholder' => getTitle('products.price')]) !!}
                                 <span class="input-group-addon">VND</span>
                             </div>
                         </div>
@@ -33,7 +33,7 @@
                             <span class="required"></span>
                             <div class="input-group">
                                 <span class="input-group-addon">1</span>
-                                {!! Form::text('quantity', null, ['class' => 'form-control', 'placeholder' => getTitle('products.quantity')]) !!}
+                                {!! Form::text('ProductPrice['.$index.'][quantity]', null, ['class' => 'form-control', 'placeholder' => getTitle('products.quantity')]) !!}
                             </div>
                         </div>
                     </div>
@@ -43,7 +43,7 @@
                             <span class="required"></span>
                             <div class="input-group">
                                 <span class="input-group-addon"><i class="fa fa-map"></i></span>
-                                {!! Form::select('origin', $params['origin'], null, ['class' => 'form-control']) !!}
+                                {!! Form::select('ProductPrice['.$index.'][origin]', $params['origin'], null, ['class' => 'form-control']) !!}
                             </div>
                         </div>
                     </div>
@@ -52,7 +52,7 @@
                     <div class="col-md-12">
                         <div class="form-group">
                             {!! Form::label('sale', getTitle('products.sale')) !!}
-                            {!! Form::textarea('sale', null, ['class' => 'form-control', 'placeholder' => getTitle('products.sale'), 'rows' => '3']) !!}
+                            {!! Form::textarea('ProductPrice['.$index.'][sale]', null, ['class' => 'form-control', 'placeholder' => getTitle('products.sale'), 'rows' => '3']) !!}
                         </div>
                     </div>
                 </div>
