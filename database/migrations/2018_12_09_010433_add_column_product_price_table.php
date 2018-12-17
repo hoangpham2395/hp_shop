e03f9d5f3dda0a072805e3555b03f2eefa197344
HoangPH <hoangph.paraline@gmail.com>
2018-12-17 09:16:55 +0700

    [Migrate] Update table product price

--- database/migrations/2018_12_09_010433_add_column_product_price_table.php
+++ database/migrations/2018_12_09_010433_add_column_product_price_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddColumnTableProductPrice extends Migration
+class AddColumnProductPriceTable extends Migration
 {
     /**
      * Run the migrations.
